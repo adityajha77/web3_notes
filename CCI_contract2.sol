7680f765b068997be96253160283eafcc2a3c98f
ADITYA KUMAR JHA <adityakartikey1259@gmail.com>
2025-08-20 00:11:17 +0530

    Update CCI_contract2.sol
    added payables

--- CCI_contract2.sol
+++ CCI_contract2.sol
@@ -34,3 +34,20 @@ function add() public {
     num=num+1;
 }
 }
+-------------------------------------------
+//payables
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Money{
+    uint public totalMoney;
+
+    function deposit() public payable {
+        totalMoney += msg.value;
+    }
+    function withdraw(address  payable ad)public {
+        payable(ad).transfer(totalMoney);
+        totalMoney=0;
+    }
+}
+//for withdraw and deposit money to other and its a good way to interact .
